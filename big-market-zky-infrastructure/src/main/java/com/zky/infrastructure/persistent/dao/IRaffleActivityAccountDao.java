51fbd71c5986c7f3f55722cc78491be02aef515b
zhangkaiyu <zhangkaiyu@purvar.com>
2025-12-05 16:44:54 +0800

    feat: 14:引入分库分表路由组件

---
+++ big-market-zky-infrastructure/src/main/java/com/zky/infrastructure/persistent/dao/IRaffleActivityAccountDao.java
@@ -0,0 +1,12 @@
+package com.zky.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zky
+ * @description 抽奖活动账户表
+ * @create
+ */
+@Mapper
+public interface IRaffleActivityAccountDao {
+}
---
+++ big-market-zky-infrastructure/src/main/java/com/zky/infrastructure/persistent/dao/IRaffleActivityAccountFlowDao.java
@@ -0,0 +1,12 @@
+package com.zky.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zky
+ * @description 抽奖活动账户流水表Dao
+ * @create
+ */
+@Mapper
+public interface IRaffleActivityAccountFlowDao {
+}
