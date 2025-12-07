c64fbc0197605134a4bb766c0f99f8864d2f106c
zky <2561144804@qq.com>
2025-12-07 22:58:22 +0800

    feat: 15:抽奖活动流水入库

---
+++ big-market-zky-domain/src/main/java/com/zky/domain/activity/service/RaffleActivitySupport.java
@@ -0,0 +1,37 @@
+package com.zky.domain.activity.service;
+
+import com.zky.domain.activity.model.entity.ActivityCountEntity;
+import com.zky.domain.activity.model.entity.ActivityEntity;
+import com.zky.domain.activity.model.entity.ActivitySkuEntity;
+import com.zky.domain.activity.repository.IActivityRepository;
+import com.zky.domain.activity.service.rule.factory.DefaultActivityChainFactory;
+
+/**
+ * @author zky
+ * @description 抽奖活动的支撑类
+ * @create
+ */
+public class RaffleActivitySupport {
+
+    protected DefaultActivityChainFactory defaultActivityChainFactory;
+
+    protected IActivityRepository activityRepository;
+
+    public RaffleActivitySupport(IActivityRepository activityRepository, DefaultActivityChainFactory defaultActivityChainFactory) {
+        this.activityRepository = activityRepository;
+        this.defaultActivityChainFactory = defaultActivityChainFactory;
+    }
+
+    public ActivitySkuEntity queryActivitySku(Long sku) {
+        return activityRepository.queryActivitySku(sku);
+    }
+
+    public ActivityEntity queryRaffleActivityByActivityId(Long activityId) {
+        return activityRepository.queryRaffleActivityByActivityId(activityId);
+    }
+
+    public ActivityCountEntity queryRaffleActivityCountByActivityCountId(Long activityCountId) {
+        return activityRepository.queryRaffleActivityCountByActivityCountId(activityCountId);
+    }
+
+}
