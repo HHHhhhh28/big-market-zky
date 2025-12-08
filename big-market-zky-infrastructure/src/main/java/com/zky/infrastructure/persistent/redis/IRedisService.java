f6b9293d4f66fc9f5174ca929d1838b0ca38a7ad
zhangkaiyu <zhangkaiyu@purvar.com>
2025-12-08 17:28:52 +0800

    feat: 16:引入MQ处理活动SKU库存一致性

--- big-market-zky-infrastructure/src/main/java/com/zky/infrastructure/persistent/redis/IRedisService.java
+++ big-market-zky-infrastructure/src/main/java/com/zky/infrastructure/persistent/redis/IRedisService.java
@@ -2,6 +2,8 @@ package com.zky.infrastructure.persistent.redis;
 
 import org.redisson.api.*;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Redis 服务
  *
@@ -262,5 +264,8 @@ public interface IRedisService {
     Long getAtomicLong(String key);
 
     Boolean setNx(String key);
+
+    Boolean setNx(String key, long expired, TimeUnit timeUnit);
+
 
 }
