69175f26ebd79920bbdfe9cfc28ee798451e64d1
zhangkaiyu <zhangkaiyu@purvar.com>
2026-01-13 14:29:33 +0800

    feat: 28.2:引入Nacos+Dubbo框架

--- big-market-zky-api/src/main/java/com/zky/trigger/api/dto/RaffleAwardListRequestDTO.java
+++ big-market-zky-api/src/main/java/com/zky/trigger/api/dto/RaffleAwardListRequestDTO.java
@@ -2,13 +2,15 @@ package com.zky.trigger.api.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author zky
  * @description 抽奖奖品列表，请求对象
  * @create
  */
 @Data
-public class RaffleAwardListRequestDTO {
+public class RaffleAwardListRequestDTO implements Serializable {
 
     // 用户ID
     private String userId;
--- big-market-zky-api/src/main/java/com/zky/trigger/api/dto/UserActivityAccountRequestDTO.java
+++ big-market-zky-api/src/main/java/com/zky/trigger/api/dto/UserActivityAccountRequestDTO.java
@@ -2,13 +2,15 @@ package com.zky.trigger.api.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author zky
  * @description 用户活动账户请求对象
  * @create
  */
 @Data
-public class UserActivityAccountRequestDTO {
+public class UserActivityAccountRequestDTO implements Serializable {
 
     /**
      * 用户ID
